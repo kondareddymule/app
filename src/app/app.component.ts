1b62aaf56b0ccc8e4121b5f2fa5d414cd147ac52
kondareddymule <MuleKondaReddy@itorizon.com>
2025-07-23 10:22:23 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    visible: boolean = false
+    text: string = ""
+    datasource: any
+
+
+    options: any[] = [{name: "Reddy", code: "Reddy"}];
+    labels = ["Reddy"]
+
+    parent: any
+
+    show() {
+      this.visible = true
+    }
+
+    ngOnInit() {
+      this.datasource = {name: "Reddy"};
+    }
+    ngAfterViewInit(): void {
+      this.rerender()
+      console.log(this.datasource)
+    }
+
+    rerender() {
+
+      ($('#chart-container') as any).orgchart({
+        'data': this.datasource,
+        nodeTitle: 'name',
+        pan: true,
+        zoom: true
+
+      });
+    }
+
+    onSumbit() {
+
+      if(this.labels.includes(this.text)) {
+        alert("Please Enter Unique node name")
+        return
+      }
+
+      if(this.parent === "" || this.text === "") {
+        alert("Please Enter both fields")
+        return 
+      }
+      let element = document.getElementById('chart-container')
+      if(element) {
+        element.innerHTML = ""
+      }
+      this.labels.push(this.text)
+      this.options = []
+      for (let i of this.labels) {
+        this.options.push({name: i, code: i})
+      }
+      this.searchNode(this.datasource, this.parent, this.text)
+      console.log(this.datasource)
+      this.rerender()
+      this.visible = false
+      this.parent = null
+      this.text = ""
+    }
+
+    searchNode(node: any, parent: string, text: string) {
+      if (node.name === parent) {
+        if (!node.children) {
+          node.children = [];
+        }
+        node.children.push({ name: text, 'relationship': '110'});
+        return true;
+      }
+
+      if (node.children) {
+        for (let child of node.children) {
+          if (this.searchNode(child, parent, text)) {
+            return true;
+          }
+        }
+      }
+
+      return false;
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { DialogModule } from 'primeng/dialog';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DropdownModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    DialogModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
